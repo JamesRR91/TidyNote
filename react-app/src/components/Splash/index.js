e59dd2eaefaa431f54ca588607c139339c888513
Sumit-dey <deysumit417@gmail.com>
2022-01-10 20:27:08 -0500

    styling splash page

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -9,7 +9,9 @@ export default function Splash(){
     return(
         <div className="parent">
             <div className="SplashBox1">
-
+                <h3 className="welcome">Welcome to TidyNote</h3>
+                <h4 className="quote">"Tame your Work, Organise your Life"</h4>
+                <h4 className="quote2">Remember everything and tackle any project with your notes, tasks, and schedule all in one place.</h4>
             </div>
             <div className="SplashBox2">
 
