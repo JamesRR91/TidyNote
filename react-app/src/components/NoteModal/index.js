1d743040395d896f8db06e6e9add53cbfab075a3
Sumit-dey <deysumit417@gmail.com>
2022-03-02 03:18:17 -0500

    note modal implement

---
+++ react-app/src/components/NoteModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import PostNote from './LeaveNote';
+import './LeaveNote.css'
+
+function LeaveNoteModal() {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setShowModal(true)} className='leave-review'>Add a Note</button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <PostNote hideForm={() => setShowModal(false)} />
+                </Modal>
+            )}
+        </>
+    );
+}
+
+export default LeaveNoteModal;
--- react-app/src/components/PostNote/PostNote.js
+++ react-app/src/components/PostNote/PostNote.js
@@ -1,114 +1,114 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { createNote } from '../../store/note';
-import { getBooks } from '../../store/book';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import {RiSave3Fill} from 'react-icons/ri'
+// import React from 'react';
+// import { useEffect, useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { createNote } from '../../store/note';
+// import { getBooks } from '../../store/book';
+// import { CKEditor } from '@ckeditor/ckeditor5-react';
+// import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import {RiSave3Fill} from 'react-icons/ri'
 
-import './PostNote.css';
+// import './PostNote.css';
 
 
-export default function PostNote(){
-    const booksObj = useSelector((state) => state.book.entries);
-    const books = Object.values(booksObj);
-    const firstBook=books[0]?.id;
-    const dispatch= useDispatch();
-    useEffect(() => {
-        dispatch(getBooks());
-    }, [dispatch]);
-    // const firstBookId=firstBook.id;
-    const [name, setName] = useState("");
-    const [text, setText] = useState("");
-    // console.log('TESTING TESTING', books[0].book_name)
-    const [bookId, setBookId]=useState(firstBook);
-    // const booksObj = useSelector((state) => state.book.entries);
-    // const books = Object.values(booksObj);
-    // useEffect(() => {
-    //     dispatch(getBooks());
-    // }, [dispatch]);
-      const reset = () => {
-        setName("");
-        setText("");
-        setBookId(firstBook);
-      };
+// export default function PostNote(){
+//     const booksObj = useSelector((state) => state.book.entries);
+//     const books = Object.values(booksObj);
+//     const firstBook=books[0]?.id;
+//     const dispatch= useDispatch();
+//     useEffect(() => {
+//         dispatch(getBooks());
+//     }, [dispatch]);
+//     // const firstBookId=firstBook.id;
+//     const [name, setName] = useState("");
+//     const [text, setText] = useState("");
+//     // console.log('TESTING TESTING', books[0].book_name)
+//     const [bookId, setBookId]=useState(firstBook);
+//     // const booksObj = useSelector((state) => state.book.entries);
+//     // const books = Object.values(booksObj);
+//     // useEffect(() => {
+//     //     dispatch(getBooks());
+//     // }, [dispatch]);
+//       const reset = () => {
+//         setName("");
+//         setText("");
+//         setBookId(firstBook);
+//       };
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
+//       const handleSubmit = (e) => {
+//         e.preventDefault();
 
-        if (!name) {
-          return alert('Your note must have a name.')
-        }
+//         if (!name) {
+//           return alert('Your note must have a name.')
+//         }
 
-        if(!text) {
-            return alert('Your note must have content.')
-        }
+//         if(!text) {
+//             return alert('Your note must have content.')
+//         }
 
-        if(bookId===undefined) {
-            return alert('Please select a notebook!')
-        }
+//         if(bookId===undefined) {
+//             return alert('Please select a notebook!')
+//         }
 
-        const newNote = {
-          note_name: name,
-          note_text: text,
-          bookId
-        };
-        dispatch(createNote(newNote));
-        reset();
-      };
+//         const newNote = {
+//           note_name: name,
+//           note_text: text,
+//           bookId
+//         };
+//         dispatch(createNote(newNote));
+//         reset();
+//       };
 
-      const editorConfiguration = {
-        toolbar: [
-          'heading', '|',
-          'bold', 'italic', '|',
-          'link', '|',
-          'bulletedList', 'numberedList', '|',
-          'insertTable', '|',
-          'blockQuote', '|',
-          'undo', 'redo'
-        ], shouldNotGroupWhenFull: true
-     };
+//       const editorConfiguration = {
+//         toolbar: [
+//           'heading', '|',
+//           'bold', 'italic', '|',
+//           'link', '|',
+//           'bulletedList', 'numberedList', '|',
+//           'insertTable', '|',
+//           'blockQuote', '|',
+//           'undo', 'redo'
+//         ], shouldNotGroupWhenFull: true
+//      };
 
-    return(
-        <div className="PostNote">
-          <form className='input-data' onSubmit={handleSubmit}>
-            <input className='input-data'
-              id="newNoteOrEditNoteBox"
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              placeholder="New Note"
-              name="name"
-              autocomplete="off"
-              required
-            />
-          <CKEditor  className='input-data'
-              editor={ClassicEditor}
-              config={ editorConfiguration }
-              data={text}
-              onChange={(event, editor) => {
-              const data = editor.getData();
-              setText(data)
+//     return(
+//         <div className="PostNote">
+//           <form className='input-data' onSubmit={handleSubmit}>
+//             <input className='input-data'
+//               id="newNoteOrEditNoteBox"
+//               type="text"
+//               onChange={(e) => setName(e.target.value)}
+//               value={name}
+//               placeholder="New Note"
+//               name="name"
+//               autocomplete="off"
+//               required
+//             />
+//           <CKEditor  className='input-data'
+//               editor={ClassicEditor}
+//               config={ editorConfiguration }
+//               data={text}
+//               onChange={(event, editor) => {
+//               const data = editor.getData();
+//               setText(data)
 
-            }}
-            required
-          />
-            <div className='editing-note-bar'>
-            <select className='input-data'
-            onChange={(e)=>setBookId(e.target.value)}
-            value={bookId}
-            required
-            >
-            <option value=''>Please Select A Notebook</option>
+//             }}
+//             required
+//           />
+//             <div className='editing-note-bar'>
+//             <select className='input-data'
+//             onChange={(e)=>setBookId(e.target.value)}
+//             value={bookId}
+//             required
+//             >
+//             <option value=''>Please Select A Notebook</option>
 
-            {books.map(({ id, book_name }) => (
-                <option value={id}>{book_name}</option>))}
-            </select>
+//             {books.map(({ id, book_name }) => (
+//                 <option value={id}>{book_name}</option>))}
+//             </select>
 
-            <button className='save-note-button' type="submit"><RiSave3Fill size='25px'/></button>
-            </div>
-          </form>
-        </div>
-        );
-};
+//             <button className='save-note-button' type="submit"><RiSave3Fill size='25px'/></button>
+//             </div>
+//           </form>
+//         </div>
+//         );
+// };
