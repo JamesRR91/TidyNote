9c2cfa2b8c8bb134cad3e044f2686b06840662eb
NawalJAhmed <11577850+NawalJAhmed@users.noreply.github.com>
2022-03-03 19:04:09 -0500

    formatting

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -13,7 +13,7 @@ export default function Splash() {
       <div>
         <div className='parent'>
           <div className='BoxOne'>
-            <h3 className='welcome'>Welcome to TidyNote</h3>
+            <h3 className='welcome'>Welcome to TidyNote.</h3>
             <h4 className='topOne'>Tame Your Work. Organize Your Life.</h4>
             <h4 className='topTwo'>
               Remember everything and tackle any project with your books, notes, and tags all in one place.
@@ -24,6 +24,32 @@ export default function Splash() {
               src='https://catpedia.net/wp-content/uploads/2018/01/Chartreux-cat.jpg'
               className='laptopImage'
             ></img>
+              <div className='BoxTwoQuotes'>
+                <h4 className='sideQuoteHeader'>
+                  WORK ANYWHERE
+                </h4>
+                <h4 className='sideQuoteParagraph'>
+                Keep important info handy—your notes be saved online.
+                </h4>
+                <h4 className='sideQuoteHeader'>
+                REMEMBER EVERYTHING
+                </h4>
+                <h4 className='sideQuoteParagraph'>
+                Make notes more useful by adding text, images, audio, scans, PDFs, and documents.
+                </h4>
+                <h4 className='sideQuoteHeader'>
+                TURN TO-DO INTO DONE
+                </h4>
+                <h4 className='sideQuoteParagraph'>
+                Bring your notes, tasks, and schedules together to get things done more easily.
+                </h4>
+                <h4 className='sideQuoteHeader'>
+                FIND THINGS FAST
+                </h4>
+                <h4 className='sideQuoteParagraph'>
+                Get what you need, when you need it with powerful, flexible search capabilities.
+                </h4>
+              </div>
           </div>
         </div>
         <Footer />
